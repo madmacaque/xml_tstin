4d98f17310609ec5ff1f8e243e533de15f66101f
Mad Macaque <speedy.np@gmail.com>
2018-09-13 21:45:56 -0400

    asks user for new customer

--- src/xml_modin.java
+++ src/xml_modin.java
@@ -13,34 +13,49 @@ public class xml_modin {
     public static void main (String[] args){
 
         try{
+            //parsing the original document
             File inputFile=new File("src/modxml.xml");
             DocumentBuilderFactory dbFactory= DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder=dbFactory.newDocumentBuilder();
             Document doc=dBuilder.parse(inputFile);
 
-            addNewCustomer(doc);
+            askUser(doc);
 
-
-            //writing it to modxml.xml file
+            //writing the modifications to modxml.xml file
             TransformerFactory transformerFactory= TransformerFactory.newInstance();
             Transformer transformer=transformerFactory.newTransformer();
             DOMSource source=new DOMSource(doc);
             StreamResult result= new StreamResult(new File("src/modxml.xml"));
             transformer.transform(source, result);
 
-
-
-
         } catch (Exception e){
             e.printStackTrace();
         }
     }
 
+    //asks user if they'd like to add a new customer.
+    //responses accepted yes YES y Y no n N NO
+    static void askUser (Document doc){
+        Scanner doWhat=new Scanner(System.in);
+        System.out.println("Would you like to add a new customer? ");
+        String response=doWhat.next();
+        if (response.equals("y") || response.equals("Y") || response.equals("YES") || response.equals("yes")){
+            addNewCustomer(doc);
+            return;
+        }
+        else if (response.equals("no") || response.equals("NO") || response.equals("n") || response.equals("N")){
+            return;
+        }
+        else
+            System.out.println("Response not recognized. Please try again.");
+            askUser(doc);
+    }
+
+
     //add new customer with user input name, ph, and id
     static void addNewCustomer(Document doc) {
         //root customers element
         Node rootElement=doc.getFirstChild();
-
 
         //check latest ID
         NodeList listofCustomers=doc.getElementsByTagName("customer");
@@ -51,6 +66,7 @@ public class xml_modin {
         rootElement.appendChild(newCustomer);
 
         //attribute for customer element: ID
+        //ID number goes up by one each time we add a customer
         Attr attr = doc.createAttribute("ID");
         attr.setValue(Integer.toString(latestID+1));
         newCustomer.setAttributeNode(attr);
